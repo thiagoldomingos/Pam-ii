5a08bd6fd7d985603a2220851f101efa2d13dcd6
Giovanni <giovanni.seloto.silva@gmail.com>
2024-08-26 17:48:28 -0300

    Ultimas atualizações do mini projeto

--- exercicio_1/app/_layout.tsx
+++ exercicio_1/app/_layout.tsx
@@ -1,9 +1,24 @@
-import { Stack } from "expo-router";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Index from './index';
+import Welcome from './welcome';
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Stack.Navigator >
+      <Stack.Screen
+        name='index'
+        component={Index}
+        options={{headerShown: false}}
+      />
+
+      <Stack.Screen 
+        name='Welcome'
+        component={Welcome}
+      />
+    </Stack.Navigator>
   );
 }
--- exercicio_1/app/index.tsx
+++ exercicio_1/app/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, View, Button, TextInput, StyleSheet, 
-  SafeAreaView, Alert, Image} from "react-native";
+  SafeAreaView, Alert} from "react-native";
 
   //Importa uma função utilizada pelo FireBase para criar um usuário com email e senha
 import {createUserWithEmailAndPassword} from 'firebase/auth'
@@ -11,21 +11,30 @@ import{auth} from "../src/services/firebase";
   //Importação da biblioteca para imgs animadas
 import * as Animatable from 'react-native-animatable';
 
+import { useRouter } from 'expo-router';
+
 export default function Index() {
   
   //Criação de estados para armazenar emails e senhas
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
-
+  const router = useRouter();
   
   // Criação de função assíncrona
   async function cadastro() {
     // Chama a função do FireBase para criar um novo usuario e senha
+    try{
     await createUserWithEmailAndPassword(auth, email, password)
-
-    Alert.alert("Sucesso!", "Usuário cadastrado com sucesso!")
-  };
-
+    //Faz a função do if
+        console.log('Cadastro bem-sucedido');
+        router.replace('/welcome');
+      }
+    //Faz a função do if else
+      catch(error){
+        console.log('Erro de cadastro:',error);
+        Alert.alert('ERRO', 'Usuário já cadastrado');
+      }
+    }
 
   return (
     <SafeAreaView style={styles.contentContainerStyle}>
@@ -65,7 +74,7 @@ export default function Index() {
     
       <View style={styles.container}>
         <Button
-          title="cadastrar"
+          title="welcome"
           color="#49708a"
           onPress={()=> cadastro()}
         />
@@ -91,15 +100,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title:{
-    fontFamily: 'Arial', 
     fontSize: 30,
   },
   input:{
-    height: 40, // Define a altura dentro do TextInput
+    height: 40, 
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 5,
-    paddingHorizontal: 10, // Adiciona padding horizontal dentro do TextInput
+    paddingHorizontal: 10,
     margin: 10
   },
   alertBtn:{
